10545d8dce00993a50464ff5351d481961abceef
Allen <glux7381@gmail.com>
2020-05-21 23:18:10 +0800

    Finish Sream Video Project

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
-
 class StreamShow extends React.Component {
-  componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef('videoElement');
   }
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer() {
+    const { id } = this.props.match.params;
+
+    if (this.player || !this.props.streams) {
+      return;
+    }
+
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  }
+
   render() {
     const { stream } = this.props;
     if (!stream) {
@@ -14,6 +41,12 @@ class StreamShow extends React.Component {
 
     return (
       <div>
+        <video
+          ref="videoElement"
+          id="videoElement"
+          style={{ width: '100%' }}
+          controls
+        ></video>
         <h1>{stream.title}</h1>
         <h5>{stream.description}</h5>
       </div>
